6648dc7ec6c1b1ee5aaa49712dfb5b2e4f3d3728
wehzie <39304339+Wehzie@users.noreply.github.com>
2021-04-07 15:40:25 +0200

    agent implemented

--- src/agent.py
+++ src/agent.py
@@ -6,7 +6,9 @@ class Agent:
     self.par = {
         'init_balance': 100,                # initial balance
         'init_balance_deviation': 10,       # deviation around initial balance
-        'trade_prob': 0.01                  # normalized probability of an agent to trade per epoch
+        'donate_prob': 0.01,                # normalized probability of an agent to trade per epoch
+        'min_don_amount': 1,                # the minimum amount for a donation
+        'max_frac_donated': 0.2,            # maximum fraction of current balance donated
     }
 
     # initialize an instance of an agent
@@ -14,14 +16,31 @@ class Agent:
         rnd_dev = self.par['init_balance_deviation']                                          
         self.balance: float = self.par['init_balance'] + random.uniform(-rnd_dev, rnd_dev)    # initialize balance
     
-    # evaluate the probability that an agent engages in a trade
+    # evaluate the probability that an agent engages in a donation
     # True means trade, False means don't trade
-    # trades are decided unilaterally, agents don't accept or reject a request to trade
-    def trade_decision(self) -> bool:
-        return True if random.uniform(0, 1) <= self.par['trade_prob'] else False
+    # donations are decided unilaterally, agents don't accept or reject a request to trade
+    def donation_decision(self) -> bool:
+        chance = random.uniform(0, 1) <= self.par['donate_prob']    # wether an agent donates is partially chance
+        min_balance = self.balance >= self.par['min_don_amount']    # agent has enough balance to make minimum donation
+        return True if min_balance and chance else False
 
-    # agent does a transaction with another agent with some probability
-    def trade(self, other_agent):
-        if self.trade_decision() == False: return
+    # find a partner to donate to
+    def find_donation_partner(self, agents) -> Agent:
+        agents = agents.copy()
+        agents.remove(self)             # agent removes itself from list of agents
+        return random.sample(agents)    # sample from the remaining list
+    
+    # determine amount to be donated
+    def determine_amount(self) -> float:
+        min_don = self.par['min_don_amount']                                        # minimum amount donated
+        max_don = min(self.balance, self.balance * self.par['max_frac_donated'])    # maximum amount donated
+        return random.uniform(min_don, max_don)
+        
+    # agent donates to another agent
+    def donate(self, donation_recipient, amount):
+        self.balance -= amount
+        donation_recipient.balance += amount
+
+
     
     
--- src/world.py
+++ src/world.py
@@ -1,3 +1,4 @@
+import random
 
 from agent_sybil import AgentSybil
 from agent_honest import AgentHonest
@@ -6,8 +7,8 @@ from history import History
 
 class World():
     
-    self.params: dict = {
-        'num_blocks': 100,        # number of simulated blocks
+    self.par: dict = {
+        'num_epochs': 100,        # number of simulated epochs or blocks
         'num_agents': 1000,       # number of agents in simulation
         'frac_sybils': 0.1,       # fraction of agents that are Sybils
     }
@@ -16,14 +17,24 @@ class World():
         self.agents: list = init_agents()
 
         def init_agents() -> list[object]:
-            num_agents, frac_sybils = self.params['num_agents'], self.params['frac_sybils']
+            num_agents, frac_sybils = self.par['num_agents'], self.par['frac_sybils']
             agents_honest = [AgentHonest() for i in range(num_agents * (1-frac_sybils))]
             agents_sybil = [AgentSybil() for i in range(num_agents * frac_sybils)]
 
-    
+    # generate transaction history for a set number of epochs
     def run(self):
-        NotImplemented
+        for in range(self.par['num_epochs']):
 
+            random.shuffle(self.agents)         # shuffle the agent list
+
+            for agent in self.agents:
+
+                if agent.donation_decision() == False: continue       # if agent chooses to want to donate
+                
+                partner = agent.find_donation_partner(self.agents)             # then it will try to find a partner to donate to
+                amount = agent.determine_amount()                   # then it will determine an amount to donate
+                agent.donate(partner, amount)                       # an amount is transferred to the partner unilaterally (without its agreement)
+            
     def save(self):
         NotImplemented
 
